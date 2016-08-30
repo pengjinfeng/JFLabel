84feca847f603c7b3de20de2998dc8cd844e29e1
pengjinfeng <peng_jinfeng@163.com>
2016-08-30 11:14:51 +0800

    Attribute
    Attribute

--- JFLabel/JFLabel/JFLabel.h
+++ JFLabel/JFLabel/JFLabel.h
@@ -6,8 +6,36 @@
 //  Copyright © 2016年 pengjf. All rights reserved.
 //
 
+#define degree(d) (M_PI * d / 180.0f)
 #import <UIKit/UIKit.h>
+typedef NS_ENUM(NSInteger,JFLabelAttribute){
+    //颜色
+    JFLabelAttributeColor = 0,
+    //字体大小
+    JFLabelAttributeFont,
+    //斜体
+    JFLabelAttributeObliqueness,
+    //字间距
+    JFLabelAttributeKern,
+    //删除线
+    JFLabelAttributeDeleteLine,
+    //下划线
+    JFLabelAttributeUnderLine,
+    
+};
+
 
 @interface JFLabel : UILabel
+//行间距
+@property (nonatomic,assign)CGFloat lineSpace;
+//首行缩进
+@property (nonatomic,assign)CGFloat headIndentSpace;
+//段落间距
+@property (nonatomic,assign)CGFloat paragaraphSpace;
+
+
+- (void)addJFLabelAttribute:(JFLabelAttribute)attrStyle value:(nonnull id)value range:(NSRange)range;
+
+-(void)addImage:(nonnull UIImage *)image size:(CGSize)size offset:(UIOffset)offset location:(NSUInteger)location;
 
 @end
--- JFLabel/JFLabel/JFLabel.m
+++ JFLabel/JFLabel/JFLabel.m
@@ -8,14 +8,152 @@
 
 #import "JFLabel.h"
 
+
+@interface JFLabel()
+
+@property (nonatomic,strong)NSMutableAttributedString *jfAttributeText;
+
+@property (nonatomic,strong)NSMutableParagraphStyle *style;
+
+@end
+
 @implementation JFLabel
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)initWithFrame:(CGRect)frame{
+    if (self = [super initWithFrame:frame]) {
+        [self jfLabelPrepare];
+    }
+    return self;
 }
-*/
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder{
+    if (self = [super initWithCoder:aDecoder]) {
+        [self jfLabelPrepare];
+        //xib文件是不会执行set方法
+        [self setText:self.text];
+    }
+    return self;
+}
+//初始化设置
+- (void)jfLabelPrepare{
+    self.numberOfLines = 0;
+    self.textAlignment = NSTextAlignmentCenter;
+}
+//懒加载style
+- (NSMutableParagraphStyle *)style{
+    if (_style == nil) {
+        NSMutableParagraphStyle *style = [[NSMutableParagraphStyle alloc] init];
+        _style = style;
+        //行间距
+        style.lineSpacing = 0.0f;
+        //段落间距
+        style.paragraphSpacing = 0.0f;
+        //缩进
+        style.firstLineHeadIndent = 0.0f;
+        CGFloat height  = self.font.pointSize;
+        style.minimumLineHeight = height;
+        style.lineHeightMultiple = 0.0f;
+        
+    }
+    return _style;
+}
+
+
+
+
+
+//设置text的时候讲text转化为我们想要的可变字符串
+- (void)setText:(NSString *)text{
+    [super setText:text];
+    
+    NSMutableAttributedString *attriString = [[NSMutableAttributedString alloc] initWithString:text];
+    self.jfAttributeText =attriString;
+//    self.attributedText = attriString;
+}
+
+
+- (void)addJFLabelAttribute:(JFLabelAttribute)attrStyle value:(nonnull id)value range:(NSRange)range{
+    switch (attrStyle) {
+        case JFLabelAttributeColor:
+            //颜色
+            [self addAttrWithName:NSForegroundColorAttributeName value:value range:range];
+            break;
+        case JFLabelAttributeFont:
+            [self addAttrWithName:NSFontAttributeName value:value range:range];
+            break;
+        case JFLabelAttributeKern:
+           [self addAttrWithName:NSKernAttributeName value:value range:range];
+            break;
+        case JFLabelAttributeObliqueness:
+            [self addAttrWithName:NSObliquenessAttributeName value:value range:range];
+            break;
+        case JFLabelAttributeDeleteLine:
+            [self addAttrWithName:NSStrikethroughStyleAttributeName value:@(YES) range:range];
+            [self addAttrWithName:NSStrikethroughColorAttributeName value:value range:range];
+            break;
+        case JFLabelAttributeUnderLine:
+            [self addAttrWithName:NSUnderlineStyleAttributeName value:@(NSUnderlineStyleSingle) range:range];
+            [self addAttrWithName:NSUnderlineColorAttributeName value:value range:range];
+            break;
+        default:
+            break;
+    }
+}
+
+-(void)addAttrWithName:(NSString *)name value:(id)value range:(NSRange)range{
+    
+    @try {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.jfAttributeText addAttribute:name value:value range:range];
+            self.attributedText=self.jfAttributeText;
+        });
+    } @catch (NSException *exception) {
+        NSLog(@"异常信息===%@===",exception);
+    } @finally {
+//        NSAssert(NO, @"Value的值是不可以为nil的");
+    }
+    
+    
+}
+
+
+-(void)addImage:(nonnull UIImage *)image size:(CGSize)size offset:(UIOffset)offset location:(NSUInteger)location{
+    
+    NSTextAttachment *attachment = [[NSTextAttachment alloc] init];
+    //设置图片
+    attachment.image = image;
+    
+    if (CGSizeEqualToSize(size, CGSizeZero)) {
+        size = CGSizeMake(self.font.lineHeight, self.font.lineHeight);
+    }
+    //设置大小
+    attachment.bounds = (CGRect){
+        CGPointMake(offset.horizontal, offset.vertical),size};
+    NSAttributedString *texts = [NSAttributedString attributedStringWithAttachment:attachment];
+    
+    [self.jfAttributeText insertAttributedString:texts atIndex:location];
+    
+}
+/** 直接设置样式，不会导致界面UI变化，需要手动调用更新 */
+-(void)updateLabelStyle{
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
