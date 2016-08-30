162112bd9e40c380c4d9ce800e3bd773b0cabca4
pengjinfeng <peng_jinfeng@163.com>
2016-08-30 08:40:14 +0800

    JFLabel
    label

---
+++ JFLabel/JFLabel/JFLabel.h
@@ -0,0 +1,13 @@
+//
+//  JFLabel.h
+//  JFLabel
+//
+//  Created by apple on 16/8/30.
+//  Copyright © 2016年 pengjf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JFLabel : UILabel
+
+@end
---
+++ JFLabel/JFLabel/JFLabel.m
@@ -0,0 +1,21 @@
+//
+//  JFLabel.m
+//  JFLabel
+//
+//  Created by apple on 16/8/30.
+//  Copyright © 2016年 pengjf. All rights reserved.
+//
+
+#import "JFLabel.h"
+
+@implementation JFLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
